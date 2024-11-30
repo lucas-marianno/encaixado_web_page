91a73b36918931bdd80b695a24c3c0f40b76f14b
Lucas Marianno <lucas.marianno94@gmail.com>
2024-11-30 17:36:56 -0300

    v0.7.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1332006413"
+    serviceWorkerVersion: "2701291075"
   }
 });
